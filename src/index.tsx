510e3538621188c6059ec03fddc3007f3b76e8e5
hlowso <h.canderleigh@gmail.com>
2023-07-17 01:21:56 -0400

    removed all logic from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { Contexts } from "frontend-common";
-import components from "./components";
 import Wrapper from "./Wrapper";
 import "./index.css";
 
@@ -9,16 +6,4 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(
-  <React.StrictMode>
-    <Contexts.FrontendProvider
-      serverUrl={process.env.REACT_APP_SERVER_URL!}
-      apiBaseUrl={
-        process.env.REACT_APP_SERVER_URL! + process.env.REACT_APP_API_BASE_URL!
-      }
-      components={components}
-    >
-      <Wrapper />
-    </Contexts.FrontendProvider>
-  </React.StrictMode>
-);
+root.render(<Wrapper />);
